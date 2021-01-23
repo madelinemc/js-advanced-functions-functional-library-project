f32472e27d2a3a029ef544b7782809b0b1f3066f
madelinemc <madeline.coons@gmail.com>
2021-01-23 12:41:28 -0600

    commit tree c89fb3e2bc7a4c72cc6854d913df34c2adbbf568

--- index.js
+++ index.js
@@ -4,17 +4,31 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      for (let element of Object.values(collection)) {
+        callback(element)
+      } 
+      return collection;
     },
 
-    map: function() {
 
+    map: function(collection, callback) {
+      let newCollection = [];
+      for (let element of Object.values(collection)) {
+        newCollection.push(callback(element))
+      }
+      return newCollection;
     },
 
-    reduce: function() {
 
+    reduce: function(collection, callback, acc) { //acc short for accumulator starts the initial state of the reduction and with each successive step it should accumulate the return value of callback
+      let reducedCollection = collection;
+      for (let element of Object.values(reducedCollection)) {
+        acc = callback(acc, element, reducedCollection)
+      }
+      return acc;
     },
+
 
     functions: function() {
 
